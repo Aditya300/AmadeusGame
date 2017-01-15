67c8c94c4a8da7870729e2d99777bb96f6a3518e
roozbit <roozbeh.khd@gmail.com>
2017-01-15 01:00:40 -0700

    ready to apply game logic

--- public/javascripts/program.js
+++ public/javascripts/program.js
@@ -2,6 +2,9 @@ var width = 960,
     height = 450,
     centered;
 
+var customerInterval = 5000;
+var gameArr=[];
+
 var projection = d3.geo.albersUsa()
     .scale(1020)
     .translate([width / 2, height / 2]);
@@ -43,13 +46,11 @@ d3.json("/us.json", function(error, us) {
         g.append('text').attr("transform", function() {return "translate(" + projection([lng,lat]) + ")";}).text(airports[i][5]).style("font-size","10px");
     }
     count = airports.length-1;
-    //window.execTimer = setTimeout(setCountToZero, 2000);
     var followingDay = new Date(+new Date + 86400000);
     var fortnightAway = new Date(+new Date + 12096e5/2);
     var iataCodes = airports.map((e) => e[5]);
-    var ajaxArr, pathArr;
+    var ajaxArr;
     ajaxArr=[];
-    pathArr=[];
 
     for(var i=count;i>0;i--) {
         for (var j = count - 1; j > 0; j--) {
@@ -65,48 +66,89 @@ d3.json("/us.json", function(error, us) {
             data = JSON.parse(data);
             var p1lat = airports[iataCodes.indexOf(data.origin)][1];
             var p1lng = airports[iataCodes.indexOf(data.origin)][2];
-            var p2lat = airports[iataCodes.indexOf(data.results[0].destination)][1];
-            var p2lng = airports[iataCodes.indexOf(data.results[0].destination)][2];
-            var price = parseFloat(data.results[0].price);
-            var lineData = [{"x": parseFloat(p1lat), "y": parseFloat(p1lng)},
-                {"x": parseFloat(p2lat), "y": parseFloat(p2lng)}];
-            var geoMultiPoint = {
-                "type": "LineString",
-                "coordinates": [
-                    [
-                        p1lng,
-                        p1lat
-                    ],
-                    [
-                        p2lng,
-                        p2lat
+            for(var k=0;k<data.results.length;k++) {
+                var p2lat = (airports[iataCodes.indexOf(data.results[k].destination)][1]);
+                var p2lng = (airports[iataCodes.indexOf(data.results[k].destination)][2]);
+                var price = (parseFloat(data.results[k].price));
+
+                //gameArr
+                var lineData = [{"x": parseFloat(p1lat), "y": parseFloat(p1lng)},
+                    {"x": parseFloat(p2lat), "y": parseFloat(p2lng)}];
+                var geoMultiPoint = {
+                    "type": "LineString",
+                    "coordinates": [
+                        [
+                            p1lng,
+                            p1lat
+                        ],
+                        [
+                            p2lng,
+                            p2lat
+                        ]
                     ]
-                ]
-            };
-            g.append("path")
-                .attr("class","routes")
-                .datum(geoMultiPoint.coordinates)
-                .attr("d", function(c) {
-                    var d = {
-                        source: projection(c[0]),
-                        target: projection(c[1])
-                    };
-                    var dx = d.target[0] - d.source[0],
-                        dy = d.target[1] - d.source[1],
+                };
+
+                g.append("svg:defs").selectAll("marker")
+                    .data(["end"])
+                    .enter().append("svg:marker")
+                    .attr("id", String)
+                    .attr("stroke", "blue")
+                    .attr("viewBox", "0 -5 10 10")
+                    .attr("refX", 12)
+                    .attr("refY", -0.2)
+                    .attr("markerWidth", 4)
+                    .attr("markerHeight", 3)
+                    .attr("orient", "auto")
+                    .append("svg:path")
+                    .attr("d", "M0,-5L10,0L0,5");
+
+                var dr;
+                var lastRoute = g.append("path")
+                    .attr("class", "routes")
+                    .attr("price", price)
+                    .datum(geoMultiPoint.coordinates)
+                    .attr("d", function (c) {
+                        var d = {
+                            source: projection(c[0]),
+                            target: projection(c[1])
+                        };
+                        var dx = d.target[0] - d.source[0],
+                            dy = d.target[1] - d.source[1];
                         dr = Math.sqrt(dx * dx + dy * dy);
-                    return "M" + d.source[0] + "," + d.source[1] + "A" + dr + "," + dr +
-                        " 0 0,1 " + d.target[0] + "," + d.target[1];
-                })
-                .attr("stroke-width", 2)
-                .attr("stroke", "blue")
-                .attr("fill", "none");
+                        return "M" + d.source[0] + "," + d.source[1] + "A" + dr + "," + dr +
+                            " 0 0,1 " + d.target[0] + "," + d.target[1];
+                    })
+                    .attr("stroke-width", 4)
+                    .attr("stroke", "blue")
+                    .attr("fill", "none")
+                    .attr("marker-end", "url(#end)")
+                    .on("mouseover", function (d) {
+                        d3.select(this).style("stroke", "yellow");
+                        coordinates = d3.mouse(this);
+                        var x = coordinates[0];
+                        var y = coordinates[1];
+                    })
+                    .on("mouseout", function (d) {
+                        d3.select(this).style("stroke", "blue");
+                    })
+                    .on("click", function() {
+                        d3.select(this).remove();
+                    });
+                lastRoute.append("svg:title")
+                    .text(function (d) {return price;});
+            }
         }).catch(err => {
             console.log("err");
         });
     });
 
+    var customerAppear = setInterval(createCustomer, customerInterval);
+
 });
 
+function createCustomer() {
+    $("#customers").append('<a href="#">');
+}
 function ajaxGet(url) {
     return new Promise(function(resolve, reject) {
         let req = new XMLHttpRequest();
